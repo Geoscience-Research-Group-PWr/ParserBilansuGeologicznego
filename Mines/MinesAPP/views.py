840a7578f5129ed24c4d435b577b53dd852f536a
Mikolaj_Siewruk <mikolajsiewruk22@gmail.com>
2024-03-18 18:06:06 +0100

    Added html templates

--- Mines/MinesAPP/views.py
+++ Mines/MinesAPP/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("HelloWorld")
+    return render(request,"MinesAPP/index.html")
+def output(request):
+    name='jogn'
+    return render(request,"MinesAPP/output.html",{"name":name})
