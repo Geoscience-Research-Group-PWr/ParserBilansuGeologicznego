1ab63403727347226c627fbbf17e913b726e0fc1
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-03-24 19:55:09 +0100

    Polaczenie z bazą MongoDB i searching

--- Mines/MinesAPP/Database.py
+++ Mines/MinesAPP/Database.py
@@ -1,1 +1,18 @@
 import pymongo
+import datetime
+
+class Database:
+    def __init__(self):
+        self.connection=pymongo.MongoClient('mongodb+srv://mikolajsiewruk222:Mikis2003@parser.1gvwkzh.mongodb.net/?retryWrites=true&w=majority&appName=parser')
+        self.db=self.connection['parser']
+        self.collection=self.db['Kopalnie']
+
+    def search(self,name,start=0,end=datetime.date.today().year):
+        query={"$and":[{"name":name},{"year":{"$gt":f'{start}'}},{"year":{"$lt":f'{end}'}}]}
+        return self.collection.find(query)
+
+s=Database()
+res=s.search("Jedlinka",2010,2024)
+
+for x in res:
+    print(x)
--- Mines/MinesAPP/views.py
+++ Mines/MinesAPP/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django import forms
-from django.http import HttpResponse
 
 tasks=['x','y','z']
 class DataForm(forms.Form):
