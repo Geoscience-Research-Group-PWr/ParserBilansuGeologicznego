4728f9b75e8c540cae987c1c1530a9827db44691
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-03-25 22:23:46 +0100

    Wielkie litery przy dodawaniu do bazy

--- Parser.py
+++ Parser.py
@@ -134,51 +134,51 @@ class Parser:
             for i in range(0, df.shape[0]):
                 if 'H E L' in files:
                     d = {
-                        'name': df.at[i, "Nazwa"],
-                        'year': year,
-                        'type': type,
-                        'more': {
-                            'stan': df.at[i, "Stan"],
+                        'Name': df.at[i, "Nazwa"],
+                        'Year': year,
+                        'Type': type,
+                        'More': {
+                            'Stan': df.at[i, "Stan"],
                             'Zas. wyd. bil. Razem': df.at[i, 'Zas. wyd. bil. Razem'],
                             'Zas. wyd. bil. A+B': df.at[i, 'Zas. wyd. bil. A+B'],
                             'Zas. wyd. bil. C': df.at[i, 'Zas. wyd. bil. C'],
-                            'wydobycie': df.at[i, 'Wydobycie']
+                            'Wydobycie': df.at[i, 'Wydobycie']
                         }
                     }
                 elif 'M E T A N  P O K Ł A D Ó W  W ĘG L A' in files:
                     d = {
-                        'name': df.at[i, "Nazwa"],
-                        'year': year,
-                        'type': type,
-                        'more': {
-                            'stan': df.at[i, "Stan"],
+                        'Name': df.at[i, "Nazwa"],
+                        'Year': year,
+                        'Type': type,
+                        'More': {
+                            'Stan': df.at[i, "Stan"],
                             'Zasoby wydobywalne bilansowe': df.at[i, 'Zasoby wydobywalne bilansowe'],
                             'Zasoby wydobywalne pozabilansowe': df.at[i, 'Zasoby wydobywalne pozabilansowe'],
                             'Zasoby przemyslowe': df.at[i, 'Zasoby przemyslowe'],
                             'Emisja z wentylacja': df.at[i, 'Emisja z wentylacja'],
-                            'wydobycie': df.at[i, 'Wydobycie']
+                            'Wydobycie': df.at[i, 'Wydobycie']
                         }
                     }
                 elif 'WĘGLE  KAMIENNE' in files:
                     d = {
-                        'name': df.at[i, "Nazwa"],
-                        'year': year,
-                        'type': type,
-                        'more': {
-                            'stan': df.at[i, "Stan"],
+                        'Name': df.at[i, "Nazwa"],
+                        'Year': year,
+                        'Type': type,
+                        'More': {
+                            'Stan': df.at[i, "Stan"],
                             'Zasoby geologiczne bilansowe Razem': df.at[i, 'Zasoby geologiczne bilansowe Razem'],
                             'Zasoby geologiczne bilansowe A+B+C1': df.at[i, 'Zasoby geologiczne bilansowe A+B+C1'],
                             'Zasoby wydobywalne bilansowe C2+D': df.at[i, 'Zasoby wydobywalne bilansowe C2+D'],
                             'Zasoby przemyslowe': df.at[i, 'Zasoby przemyslowe'],
-                            'wydobycie': df.at[i, 'Wydobycie']
+                            'Wydobycie': df.at[i, 'Wydobycie']
                         }
                     }
                 elif 'SOLANKI, WODY LECZNICZE I TERMALNE' in files:
                     d = {
-                        'name': df.at[i, "Nazwa"],
-                        'year': year,
-                        'type': type,
-                        'more': {
+                        'Name': df.at[i, "Nazwa"],
+                        'Year': year,
+                        'Type': type,
+                        'More': {
                             'Typ wody': df.at[i, "Typ wody"],
                             'Zasoby geologiczne bilansowe dyspozycyjne': df.at[
                                 i, 'Zasoby geologiczne bilansowe dyspozycyjne'],
@@ -190,11 +190,11 @@ class Parser:
                     }
                 else:
                     d = {
-                        'name': df.at[i, "Nazwa"],
-                        'year': year,
-                        'type': type,
-                        'more': {
-                            'stan': df.at[i, "Stan"],
+                        'Name': df.at[i, "Nazwa"],
+                        'Year': year,
+                        'Type': type,
+                        'More': {
+                            'Stan': df.at[i, "Stan"],
                             'Zasoby wydobywalne bilansowe': str(df.at[i, 'Zasoby wydobywalne bilansowe']),
                             'Zasoby przemyslowe': df.at[i, 'Zasoby przemyslowe'],
                             'Wydobycie': df.at[i, 'Wydobycie'],
