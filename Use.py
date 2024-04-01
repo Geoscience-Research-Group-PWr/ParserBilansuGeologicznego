7ca7ed612f7327982c40e9a3c6a3e5803b5099a2
Mikolaj_Siewruk <mikolajsiewruk22@gmail.com>
2024-04-01 22:06:55 +0200

    Multiprocessing for parsing

--- Use.py
+++ Use.py
@@ -1,5 +1,9 @@
 import logging
+import multiprocessing
+import os
 from Parser import Parser
+
+
 logger = logging.getLogger(__name__)
 
 def main():
@@ -8,6 +12,36 @@ def main():
     parser=Parser("Bilans_2012.pdf")
     parser.add_to_db()
     logger.info("Parsing finished")
+def work(file:str):
+    """
+    Full parsing with adding to DB.
+    :param file: Geological balance sheet
+    :return:
+    """
+    parser=Parser(file)
+    logging.basicConfig(filename='parser.log', format='%(asctime)s - %(message)s',level=logging.INFO,filemode="w")
+    logger.info("Parser started")
+    parser.make_directories()
+    parser.parse()
+    parser.parse_parsed()
+    parser.export_data()
+    parser.clean_csv()
+    parser.search_csv_for_errors()
+    parser.add_to_db()
+    logger.info("Parsing finished")
+
+
+def multitask():
+    """
+    Creates 4 processes for parsing 4 files in the directory.
+    Once one process for a file is finished another file is parsed.
+    :return:
+    """
+    directory = os.getcwd()
+    pdfs=[files for files in os.listdir(directory) if files.endswith(".pdf")]
+    with multiprocessing.Pool(4) as pool:
+            pool.map(work,pdfs)
+
 
 if __name__=='__main__':
-    main()
+    multitask()
